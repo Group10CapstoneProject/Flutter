cdae233ecba227ae9ea49a9a2dc39bd3acbe60c0
natanaelhn <nainggolannainggolan74@gmail.com>
2022-11-29 23:27:04 +0700

    memperbaiki offline_screen dengan memisahkan antara offline_screen dengan class_screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:capstone_alterra_flutter/provider/class_provider.dart';
 import 'package:capstone_alterra_flutter/provider/homepage_provider.dart';
 import 'package:capstone_alterra_flutter/provider/main_provider.dart';
 import 'package:capstone_alterra_flutter/screen/splash/splash_screen.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
@@ -10,6 +12,7 @@ void main() {
     providers: [
       ChangeNotifierProvider(create: (context) => MainProvider(),),
       ChangeNotifierProvider(create: (context) => HomepageProvider(),),
+      ChangeNotifierProvider(create: (context) => ClassProvider()),
     ],
     child: const MyApp()
   ));
@@ -21,6 +24,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      scrollBehavior: const MaterialScrollBehavior().copyWith(
+        dragDevices: {
+          PointerDeviceKind.mouse,
+          PointerDeviceKind.touch,
+          PointerDeviceKind.stylus,
+          PointerDeviceKind.unknown
+        }
+      ),
       theme: ThemeData(
         appBarTheme: const AppBarTheme(
           systemOverlayStyle: SystemUiOverlayStyle(
--- lib/screen/offline/offline_screen.dart
+++ lib/screen/offline/offline_screen.dart
@@ -1,7 +1,5 @@
 import 'package:capstone_alterra_flutter/screen/offline/widget/offline_card.dart';
-import 'package:date_picker_timeline/date_picker_timeline.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 class OfflineScreen extends StatefulWidget {
   const OfflineScreen({super.key});
@@ -16,183 +14,51 @@ class _OfflineScreenState extends State<OfflineScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        systemOverlayStyle: const SystemUiOverlayStyle(
-          statusBarColor: Colors.white,
-          statusBarIconBrightness: Brightness.dark,
-        ),
-        title: const Text(
-          'Kelas',
-          style: TextStyle(color: Colors.black, fontSize: 20),
-        ),
-        elevation: 0,
-      ),
-
-      // Body
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              decoration: const BoxDecoration(
-                color: Colors.white,
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey,
-                    offset: Offset(0.0, 1.0), //(x,y)
-                    blurRadius: 5.0,
-                  ),
-                ],
-              ),
-              height: 200,
-              child: Column(
-                children: [
-                  // Daftar Kelas
-                  daftarKelas(),
-
-                  // Waktu
-                  waktu(),
-                ],
-              ),
-            ),
-
-            // Filter
-            filter(),
-
-            const SizedBox(
-              height: 14.0,
-            ),
-
-            // WidgetCard
-            const OfflineCard(
-              title: 'Basic Yoga',
-              teacher: 'With Maya',
-              date: '05.30',
-              duration: '60 min',
-              availabeleSlot: '10 Slot Tersisa',
-              payment: 30000,
-              picture: 'assets/offline_page/image.png',
-            ),
-            const SizedBox(
-              height: 20.0,
-            ),
-            const OfflineCard(
-              title: 'Muay Thai',
-              teacher: 'With Kevin',
-              date: '16.00',
-              duration: '65 min',
-              availabeleSlot: '20 Slot Tersisa',
-              payment: 150000,
-              picture: 'assets/offline_page/image.png',
-            ),
-            const SizedBox(
-              height: 20.0,
-            ),
-            const OfflineCard(
-              title: 'Muay Thai',
-              teacher: 'With Kevin',
-              date: '16.00',
-              duration: '65 min',
-              availabeleSlot: '20 Slot Tersisa',
-              payment: 150000,
-              picture: 'assets/offline_page/image.png',
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  // Kelas Offline dan Online
-  Widget daftarKelas() {
-    return Container(
-      margin: const EdgeInsets.only(top: 16),
-      height: 49,
-      decoration: BoxDecoration(
-        border: Border.all(color: const Color(0xFFD3D3D3)),
-        color: const Color(0xFFF2F4F3),
-        borderRadius: BorderRadius.circular(40),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
+    return SingleChildScrollView(
+      child: Column(
         children: [
-          SizedBox(
-            width: 175,
-            height: 39,
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                elevation: 0,
-                backgroundColor: isSelected
-                    ? const Color(0xFFF2F4F3)
-                    : const Color(0xFF52A1A4),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-              ),
-              onPressed: () {
-                setState(() {
-                  isSelected = false;
-                });
-              },
-              child: Text(
-                'Offline',
-                style: TextStyle(
-                  color: isSelected
-                      ? const Color(0xFF52A1A4)
-                      : const Color(0xFFFDFDFD),
-                ),
-              ),
-            ),
+          // Filter
+          filter(),
+    
+          const SizedBox(
+            height: 14.0,
           ),
-          SizedBox(
-            width: 175,
-            height: 39,
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                elevation: 0,
-                backgroundColor: isSelected
-                    ? const Color(0xFF52A1A4)
-                    : const Color(0xFFF2F4F3),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-              ),
-              onPressed: () {
-                setState(() {
-                  isSelected = true;
-                });
-              },
-              child: Text(
-                'Online',
-                style: TextStyle(
-                  color: isSelected
-                      ? const Color(0xFFFDFDFD)
-                      : const Color(0xFF52A1A4),
-                ),
-              ),
-            ),
+    
+          // WidgetCard
+          const OfflineCard(
+            title: 'Basic Yoga',
+            teacher: 'With Maya',
+            date: '05.30',
+            duration: '60 min',
+            availabeleSlot: '10 Slot Tersisa',
+            payment: 30000,
+            picture: 'assets/offline_page/image.png',
+          ),
+          const SizedBox(
+            height: 20.0,
+          ),
+          const OfflineCard(
+            title: 'Muay Thai',
+            teacher: 'With Kevin',
+            date: '16.00',
+            duration: '65 min',
+            availabeleSlot: '20 Slot Tersisa',
+            payment: 150000,
+            picture: 'assets/offline_page/image.png',
+          ),
+          const SizedBox(
+            height: 20.0,
+          ),
+          const OfflineCard(
+            title: 'Muay Thai',
+            teacher: 'With Kevin',
+            date: '16.00',
+            duration: '65 min',
+            availabeleSlot: '20 Slot Tersisa',
+            payment: 150000,
+            picture: 'assets/offline_page/image.png',
           ),
         ],
-      ),
-    );
-  }
-
-  // Manggil Waktu
-  Widget waktu() {
-    return Container(
-      margin: const EdgeInsets.only(top: 30),
-      child: DatePicker(
-        DateTime.now(),
-        height: 80,
-        width: 50,
-        initialSelectedDate: DateTime.now(),
-        selectionColor: const Color(0xFF52A1A4),
-        locale: 'id_ID',
-        onDateChange: (date) {
-          selectedDate = date;
-        },
       ),
     );
   }
