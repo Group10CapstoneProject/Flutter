cc7bd910732ec5c9f93840f3cd053489ab211ec5
natanaelhn <nainggolannainggolan74@gmail.com>
2022-12-23 02:58:01 +0700

    menampilkan data kelas hari ini pada homepage dari api dan memberikan tampilan loading

--- lib/provider/homepage_provider.dart
+++ lib/provider/homepage_provider.dart
@@ -1,10 +1,15 @@
 import 'package:capstone_alterra_flutter/model/articles_model.dart';
 import 'package:capstone_alterra_flutter/model/json_model.dart';
+import 'package:capstone_alterra_flutter/model/offline_model.dart';
+import 'package:capstone_alterra_flutter/model/online_class_model.dart';
 import 'package:capstone_alterra_flutter/service/articles_service.dart';
+import 'package:capstone_alterra_flutter/service/offline_service.dart';
+import 'package:capstone_alterra_flutter/util/utils.dart';
 import 'package:flutter/material.dart';
 
 class HomepageProvider with ChangeNotifier{
 
+  ///List of iklan
   List<String> listIklan = [];
   double iklanIndex = 0;
 
@@ -25,12 +30,45 @@ class HomepageProvider with ChangeNotifier{
 
 
 
+  ///List of offline class for today
+  List<OfflineModel> listOffline = [];
+  bool offlineIsLoading = false;
+  OfflineService offlineService = OfflineService();
+  
+  Future<void> getOfflineClass() async {
+
+    listOffline.clear();
+    offlineIsLoading = true;
+    notifyListeners();
+
+    DateTime now = DateTime.now();
+    String date = Utils.timeDate(now);
+    JSONModel<List<OfflineModel>?> json = await offlineService.getOfflineClass(date, null, 'DESC');
+
+    listOffline = json.data!;
+    offlineIsLoading = false;
+    notifyListeners();
+
+  }
+
+
+
+
+
+
+
+
+
+
+  ///List of article
   List<ArticlesModel> listArticle = [];
+  bool articleIsLoading = false;
 
   ///to get all articles
   Future<void> getAllArticles() async {
 
     listArticle.clear();
+    articleIsLoading = true;
     notifyListeners();
 
     ArticlesService articlesService = ArticlesService();
@@ -40,6 +78,7 @@ class HomepageProvider with ChangeNotifier{
     if(json.statusCode == 200){
       listArticle.addAll(json.data!);
     }
+    articleIsLoading = false;
     notifyListeners();
   }
 }
